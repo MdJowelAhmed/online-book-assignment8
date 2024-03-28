db251cfc718fb024985f131c0c3c330fe12673d2
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-03-28 19:57:13 +0600

    upcoming section complete

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+<div>
+    <ul>
+        <li>Feature 1</li>
+        <li>Feature 2</li>
+        <li>Feature 3</li>
+        <li>Feature 4</li>
+        <li>Feature 5</li>
+    </ul>
+</div>
--- src/components/Upcoming/Upcoming.jsx
+++ src/components/Upcoming/Upcoming.jsx
@@ -2,8 +2,31 @@
 
 const Upcoming = () => {
     return (
-        <div>
-            Upcoming boooks
+        <div className=" grid grid-cols-1 lg:grid-cols-3 gap-4 mt-6">
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+                <figure><img className="h-60" src="https://boiferry.com/assets/images/redactor/md_i6ZNzk1PFDCEOE3JfF0DLPYIzDLe1MjVW9J60YTr.webp" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Ahkame bondegi</h2>
+                    <p>It is  very important for muslim include islamic knowledge</p>
+                   <h3 className="bg-secondary m-3 mx-auto p-3 rounded-xl mt-5">Upcoming soon</h3>
+                </div>
+            </div>
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+                <figure><img className="h-60" src="https://boiferry.com/assets/images/redactor/5R9zQzNiv42L3v8smYdiY7WrCPGBViHc1ehVpzss.webp" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Chukhe dekha kobore azab</h2>
+                    <p>It is  very important for muslim include akhirat life knowledge</p>
+                   <h3 className="bg-secondary m-3 mx-auto p-3 rounded-xl mt-5">Upcoming soon</h3>
+                </div>
+            </div>
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+                <figure><img className="h-60" src="https://boiferry.com/assets/images/redactor/arWzxDrZPDa4iP716uYSmGvWTxziI9vGoC4S5Ksc.webp" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Chukhe dekha kobore azab</h2>
+                    <p>It is  very important for muslim include akhirat life knowledge</p>
+                   <h3 className="bg-secondary m-3 mx-auto p-3 rounded-xl mt-5">Upcoming soon</h3>
+                </div>
+            </div>
         </div>
     );
 };
