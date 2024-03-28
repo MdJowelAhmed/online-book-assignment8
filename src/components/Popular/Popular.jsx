86150a5290922b059c9307c85bbda775bb0cce87
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-03-28 21:12:25 +0600

    added popular router

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -3,7 +3,32 @@
 const Popular = () => {
     return (
         <div>
-            this is popular pages
+           <div className=" grid grid-cols-1 lg:grid-cols-3 gap-4 mt-6">
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+                <figure><img className="h-60" src="https://static.aadi.com.bd/__sized__/products/99-6146-pri-arddaar-onnorokom-web-services-ltd-21104-1-thumbnail-500x500-70.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Bela fuarabar age</h2>
+                    <p>It is  very important for muslim include islamic knowledge</p>
+                   <h3 className="bg-success m-3 mx-auto p-3 rounded-xl mt-5">Popular Book</h3>
+                </div>
+            </div>
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+                <figure><img className="h-60" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjb4oc7NC323whrcE5M8zcFV6LvUOp40l9Kg&usqp=CAU" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Peradoxical sazid</h2>
+                    <p>It is  very important for muslim include akhirat life knowledge</p>
+                   <h3 className="bg-success m-3 mx-auto p-3 rounded-xl mt-5">Popular Book</h3>
+                </div>
+            </div>
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+                <figure><img className="h-60" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp5YC9AX7V-xTB-_7iPnYOTPOmFV-DhnO8Spuvs-tyiZXcPksk7dRyyeDSyRS3z8FxFFY&usqp=CAU" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Al-Akhida Athtahabiya </h2>
+                    <p>It is  very important for muslim include akhirat life knowledge</p>
+                   <h3 className="bg-success m-3 mx-auto p-3 rounded-xl mt-5">Popular Book</h3>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
